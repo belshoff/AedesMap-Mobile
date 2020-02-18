77741616ec0e46ebb49a25dcc3707e1129461d0f
William Vitorino <williamvitorino3@gmail.com>
2020-02-18 00:38:59 -0300

    Layout update.

--- lib/send_screen.dart
+++ lib/send_screen.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:path/path.dart' as path;
 import 'package:http/http.dart' as http;
 
+import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -18,7 +19,6 @@ class SendScreen extends StatefulWidget {
 }
 
 class _SendScreenState extends State<SendScreen> {
-
   File image;
   Position currentPosition;
   int _currentIndex = 0;
@@ -27,15 +27,18 @@ class _SendScreenState extends State<SendScreen> {
 
   final tween = MultiTrackTween(
     [
-      Track("size").add( Duration(seconds: 2), Tween(begin: 500.0, end: 0.0) ),
-      Track("rotation").add( Duration(seconds: 2), ConstantTween(0.0) )
+      Track("size").add(Duration(seconds: 2), Tween(begin: 500.0, end: 0.0)),
+      Track("rotation").add(Duration(seconds: 2), ConstantTween(0.0))
     ],
   );
 
   /// Pegar Geolocalização do dispositivo.
   _getPosition() async {
-    var location = await Geolocator().getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
-    setState(() { currentPosition = location; });
+    var location = await Geolocator()
+        .getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+    setState(() {
+      currentPosition = location;
+    });
   }
 
   _resetData() {
@@ -47,27 +50,34 @@ class _SendScreenState extends State<SendScreen> {
     });
   }
 
-
   /// Enviar imagem para o Storage..
   _sendImage() async {
+    setState(() {
+      uploading = true;
+    });
 
-    setState(() { uploading = true; });
-    
     String filename = path.basename(image.path);
-    final StorageReference store = FirebaseStorage.instance.ref().child(filename);
+    final StorageReference store =
+        FirebaseStorage.instance.ref().child(filename);
     final StorageUploadTask task = store.putFile(image);
     final StorageTaskSnapshot downloadUrl = (await task.onComplete);
     final String url = (await downloadUrl.ref.getDownloadURL());
 
-    await postImage("$url").then(
-      (_) => _resetData());
+    await postImage("$url").then((_) => _resetData());
   }
 
   Future<int> postImage(String url) async {
-    var body = { "urlImage": url, "latitude": "${currentPosition.latitude}", "longitude": "${currentPosition.longitude}" };
+    var body = {
+      "urlImage": url,
+      "latitude": "${currentPosition.latitude}",
+      "longitude": "${currentPosition.longitude}"
+    };
 
-    var response = await http.post(Uri.encodeFull("http://192.168.0.12:5000/location"), headers: {"Accept": "application/json"}, body: body);
- 
+    var response = await http.post(
+        Uri.encodeFull("http://192.168.0.12:5000/location"),
+        headers: {"Accept": "application/json"},
+        body: body);
+
     return response.statusCode;
   }
 
@@ -75,10 +85,14 @@ class _SendScreenState extends State<SendScreen> {
   _getImage(ImageSource source) async {
     _getPosition();
     var photo = await ImagePicker.pickImage(source: source);
-    photo != null ? setState(() { image = photo; }) : setState(() { }) ;
+    photo != null
+        ? setState(() {
+            image = photo;
+          })
+        : setState(() {});
   }
 
-  _setIndex( int index ) {
+  _setIndex(int index) {
     List<ImageSource> sources = [ImageSource.camera, ImageSource.gallery];
     _getImage(sources[index]);
     setState(() {
@@ -113,17 +127,13 @@ class _SendScreenState extends State<SendScreen> {
       tween: tween,
       builder: (context, animation) {
         return Transform.rotate(
-          angle: animation["rotation"],
-          child: Column(
-            children: <Widget>[
-              Image.file(
-                image,
-                height: animation["size"],
-                width: animation["size"]
-              ),
-            ],
-          )
-        );
+            angle: animation["rotation"],
+            child: Column(
+              children: <Widget>[
+                Image.file(image,
+                    height: animation["size"], width: animation["size"]),
+              ],
+            ));
       },
     );
   }
@@ -133,8 +143,12 @@ class _SendScreenState extends State<SendScreen> {
     return Align(
       alignment: Alignment(1.0, 0.8),
       child: FloatingActionButton(
-        onPressed: () { _sendImage(); },
-        child: uploading ? CircularProgressIndicator() : Icon(Icons.send, color: Colors.teal),
+        onPressed: () {
+          _sendImage();
+        },
+        child: uploading
+            ? CircularProgressIndicator()
+            : Icon(Icons.send, color: Colors.teal),
         backgroundColor: Colors.white,
       ),
     );
@@ -143,82 +157,164 @@ class _SendScreenState extends State<SendScreen> {
   Widget cancelButton() {
     return Align(
       alignment: Alignment(1.0, 1.0),
-      child: uploading ? null : FloatingActionButton(
-        onPressed: () {
-          _resetData();
-        },
-        child: Icon(Icons.cancel, color: Colors.red),
-        backgroundColor: Colors.white,
-      ),
+      child: uploading
+          ? null
+          : FloatingActionButton(
+              onPressed: () {
+                _resetData();
+              },
+              child: Icon(Icons.cancel, color: Colors.red),
+              backgroundColor: Colors.white,
+            ),
     );
   }
 
-  Widget getImage( BuildContext context ) {
-    double height = MediaQuery.of(context).size.height;
-    if ( image == null ) {
-      return Stack(
-        children: <Widget>[
-          Align(
-            alignment: Alignment.topCenter,
-            child: Text("Aedes Map", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24)),
-          ),
-          Align(
-            alignment: Alignment.bottomCenter,
-            child: Text("Selecione uma imagem usando:", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24)),
-          )
-        ],
+  Widget getImage(BuildContext context) {
+    final double height = MediaQuery.of(context).size.height;
+    final Color mainColor = Colors.grey;
+    if (image == null) {
+      return Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Spacer(),
+            InkWell(
+              onTap: () { _getImage(ImageSource.camera); },
+              child: DottedBorder(
+                color: mainColor,
+                strokeWidth: 2,
+                padding: EdgeInsets.all(4),
+                dashPattern: [9, 5],
+                child: Container(
+                  margin: EdgeInsets.only(top: 40.0, bottom: 40.0),
+                  // margin: EdgeInsets.all(20),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Container(
+                        margin: const EdgeInsets.only( right: 25.0 ),
+                        child: Icon(
+                          Icons.camera_alt,
+                          size: 50,
+                          color: mainColor
+                        ),
+                      ),
+                      Container(
+                        child: Text("Câmera",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 75,
+                            color: mainColor
+                          )
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Spacer(),
+            InkWell(
+              onTap: () { _getImage(ImageSource.gallery); },
+              child: DottedBorder(
+                color: mainColor,
+                strokeWidth: 2,
+                padding: EdgeInsets.all(4),
+                dashPattern: [9, 5],
+                child: Container(
+                  // margin: EdgeInsets.all(20),
+                  margin: EdgeInsets.only(top: 40.0, bottom: 40.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Container(
+                        margin: const EdgeInsets.only(right: 25.0),
+                        child: Icon(
+                          Icons.image,
+                          size: 50,
+                          color: mainColor
+                        ),
+                      ),
+                      Container(
+                        child: Text("Galeria",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 75,
+                            color: mainColor
+                          )
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Spacer(),
+          ],
+        ),
       );
       // return ;
-    } else if ( uploaded ) {
+    } else if (uploaded) {
       return buildAnimation();
     } else {
       return Container(
-        alignment: Alignment.topCenter,
-        child: Column(
-          children: <Widget>[
-            Image.file(image, height: height*0.75),
-            currentPosition != null ? Text("$currentPosition", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),) : Text("Can't GET position.")
-          ],
-        )
-      );
+          alignment: Alignment.topCenter,
+          child: Column(
+            children: <Widget>[
+              Image.file(image, height: height * 0.75),
+              currentPosition != null
+                  ? Text(
+                      "$currentPosition",
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                    )
+                  : Text("Can't GET position.")
+            ],
+          ));
     }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Aedes Map', style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold)),
-        centerTitle: true,
-        backgroundColor: Colors.blue,
-      ),
-      body: Container(
-        padding: const EdgeInsets.all(8),
-        child: getImage(context),
-        alignment: Alignment.topCenter,
-      ),
-      floatingActionButton: Stack(
-        children: image != null && currentPosition != null ? <Widget>[
-          sendButton(), cancelButton(),
-        ] : <Widget>[] ,
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.camera_alt),
-            title: Text("Camera"),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.image),
-            title: Text("Galeria"),
-          )
-        ],
-        selectedItemColor: Colors.white,
-        unselectedItemColor: Colors.white,
-        backgroundColor: Colors.blue,
-        currentIndex: _currentIndex,
-        onTap: _setIndex,
-      )
+        appBar: AppBar(
+          title: const Text('Aedes Map',
+              style:
+                  TextStyle(color: Colors.white, fontWeight: FontWeight.bold)),
+          centerTitle: true,
+          backgroundColor: Colors.blue,
+        ),
+        body: Container(
+          padding: const EdgeInsets.all(8),
+          child: getImage(context),
+          alignment: Alignment.topCenter,
+        ),
+        floatingActionButton: Stack(
+          children: image != null && currentPosition != null
+              ? <Widget>[
+                  sendButton(),
+                  cancelButton(),
+                ]
+              : <Widget>[],
+        ),
+        // bottomNavigationBar: BottomNavigationBar(
+        //   items: <BottomNavigationBarItem>[
+        //     BottomNavigationBarItem(
+        //       icon: Icon(Icons.camera_alt),
+        //       title: Text("Camera"),
+        //     ),
+        //     BottomNavigationBarItem(
+        //       icon: Icon(Icons.image),
+        //       title: Text("Galeria"),
+        //     )
+        //   ],
+        //   selectedItemColor: Colors.white,
+        //   unselectedItemColor: Colors.white,
+        //   backgroundColor: Colors.blue,
+        //   currentIndex: _currentIndex,
+        //   onTap: _setIndex,
+        // )
     );
   }
 }
